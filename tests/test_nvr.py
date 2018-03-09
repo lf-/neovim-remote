063790a5759ac84ee84bec36f2d63f76493e8670
lf <lf-@users.noreply.github.com>
2018-03-09 08:21:53 +0000

    Add some work on tests for the variables
    This doesn't work due to issues getting the sockets to connect
    correctly. Not sure if test suite had this issue before however.

--- tests/test_nvr.py
+++ tests/test_nvr.py
@@ -6,16 +6,32 @@ import signal
 import subprocess
 import nvr
 
-def test_open_and_write_file():
-    env = {'NVIM_LISTEN_ADDRESS': '/tmp/pytest_nvimsock'}
-    env.update(os.environ)
+env = os.environ.copy()
+env['NVIM_LISTEN_ADDRESS'] = '/tmp/pytest_nvimsock'
 
-    with subprocess.Popen(['nvim', '-nu', 'NORC', '--headless'], close_fds=True,
-                          env=env) as proc:
+def _popen_nvim(**kwargs):
+    return subprocess.Popen(['nvim', '-nu', 'NORC', '--headless'], close_fds=True,
+                            env=env, **kwargs)
+
+def test_open_and_write_file():
+    with _popen_nvim() as proc:
         time.sleep(1)
-        argv = ['nvr', '-c', 'e /tmp/pytest_file | %d | exe "norm! iabc" | w']
+        argv = ['nvr', '--nostart', '-c', 'e /tmp/pytest_file | %d | exe "norm! iabc" | w']
         nvr.main(argv=argv, env=env)
         proc.send_signal(signal.SIGTERM)
 
     with open('/tmp/pytest_file') as f:
         assert 'abc\n' == f.read()
+    os.unlink('/tmp/pytest_file')
+
+def test_set_vars():
+    with _popen_nvim() as proc:
+        # XXX: we should find a way to make this wait on neovim opening
+        time.sleep(1)
+        argv = ['nvr', '--nostart', '-V', 'aaa', 'bbb', '-V', 'ccc', 'ddd', '-c', 'e /tmp/pytest_file | %d | put =g:aaa | put =g:ccc | w']
+        nvr.main(argv=argv, env=env)
+        proc.send_signal(signal.SIGTERM)
+
+    with open('/tmp/pytest_file') as f:
+        assert 'aaaddd\n' == f.read()
+    os.unlink('/tmp/pytest_file')
